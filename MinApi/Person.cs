bdbd93b8e409dc31899d9e4ec6a1fe85352910cd
Edgar Knapp <Edgar.Knapp@simcorp.com>
2023-11-08 19:53:25 +0100

    revert to .NET 7

--- MinApi/Person.cs
+++ MinApi/Person.cs
@@ -3,10 +3,10 @@
 public record Person(string FirstName, string LastName, DateTime BirthDate)
 {
   public string BirthdayCheck()
-    {
-      var month = DateTime.UtcNow.Month;
-      var day = DateTime.UtcNow.Day;
-      var yesOrNo = month == BirthDate.Month && day == BirthDate.Day ? String.Empty : "not ";
-      return $" Today is {yesOrNo}your birthday";
-    }
+  {
+    var month = DateTime.UtcNow.Month;
+    var day = DateTime.UtcNow.Day;
+    var yesOrNo = month == BirthDate.Month && day == BirthDate.Day ? String.Empty : "not ";
+    return $" Today is {yesOrNo}your birthday";
   }
+}
